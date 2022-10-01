9105229ae8772c2c57e3be01e61f4fb396da9375
PaulQpro <paulqpro@mail.ru>
2022-10-01 18:21:47 +0300

    Started working on Calculator

---
+++ SCal/SCal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+using static System.Threading.Thread;
+using SubOS;
+namespace SubOS
+{
+    public class SCal
+    {
+        public void Menu()
+        {
+
+        }
+    }
+}
